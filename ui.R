c38861a14adab4092edd264734b04371782f4213
Nicholas Mackowski <nmack41@gmail.com>
2024-12-01 15:37:22 -0500

    start of project

---
+++ ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(ggplot2)
+
+ui <- fluidPage(
+  titlePanel("Readiness Tracker"),
+  sidebarLayout(
+    sidebarPanel(
+      h3(textOutput("current_date")),
+      numericInput(inputId = "weight", label = "Weight", min = 50, max = 500, value = 200),
+      numericInput(inputId = "sleep", label = "Sleep (0-5)", min = 0, max = 5, value = 3),
+      numericInput(inputId = "motivation_to_train", label = "Motivation to Train (0-5)", min = 0, max = 5, value = 3),
+    ),
+    mainPanel(
+      textOutput("weight"),
+      plotOutput('sample_data_plot'),
+      tableOutput('sample_data_table')
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  current_datetime <- Sys.time()
+  output$current_date <- renderText(format(current_datetime, "%m-%d-%Y"))
+  
+  ### sample data for graph
+  data("Titanic")
+  sample_data <- as.data.frame(Titanic)
+  ###
+  
+  output$weight <- renderText(input$weight)
+  
+  output$sample_data_plot <- renderPlot({
+    ggplot(sample_data, aes(x = Class, y = Freq, fill = Age)) + geom_col()})
+  
+  output$sample_data_table <- renderTable(summary(sample_data))
+  
+}
+
+
+shinyApp(ui, server)
